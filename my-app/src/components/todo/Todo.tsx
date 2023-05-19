fdddd230bef89ae953a2b15498e2de224087861c
JacquilineJaison <celinejack@gmail.com>
2023-05-19 10:47:15 -0400

    initial

---
+++ my-app/src/components/todo/Todo.tsx
@@ -0,0 +1,11 @@
+import {Button} from '@chakra-ui/react';
+
+function Todo() {
+  return (
+    <div className="">
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+}
+
+export default Todo;
